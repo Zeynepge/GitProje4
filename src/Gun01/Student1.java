34ce21f61f8ef9c8980fb2b6bb5a205e99d59e2c
Zeynepge <sgunal571@gmail.com>
2023-01-28 12:00:09 +0100

    2.task yapildi

--- src/Gun01/Student1.java
+++ src/Gun01/Student1.java
@@ -4,6 +4,7 @@ public class Student1 {
     public static void main(String[] args) {
         for (int i = 0; i < 2; i++) {
             System.out.println("merhaba");
+            System.out.println("merhaba 2");
         }
     }
 }
