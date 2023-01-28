9b0ecd62fd2aec2b17f680b5c9439ed382e9c167
Zeynepge <sgunal571@gmail.com>
2023-01-28 11:56:24 +0100

    1.task yapildi

---
+++ src/Gun01/Student1.java
@@ -0,0 +1,9 @@
+package Gun01;
+
+public class Student1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("merhaba");
+        }
+    }
+}
